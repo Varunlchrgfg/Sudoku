9abf64a3623b2b1c4b7ab104e17cf7c1adec7094
Varunbansalchrgfg <varunbansaldcsamca@gmail.com>
2024-09-04 23:29:15 +0530

    Designed_Sudoku_board

--- sudoku_updated.cpp
+++ sudoku_updated.cpp
@@ -5,15 +5,39 @@ class designSudoku{
     int main_board[9][9];
     bool row[9][9];
     bool column[9][9];
+    bool board_input[9][9];
     bool squares[3][3][9];
 };
 void initialize_board(designSudoku board){
     for(int i=0;i<9;i++){
         for(int j=0;j<9;j++){
             board.main_board[i][j]=0;
-            cout<<board.main_board[i][j];
         }
-        cout<<endl;
+    }
+
+    for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
+            board.board_input[i][j]=false;
+        }
+    }
+    for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
+            board.row[i][j]=false;
+        }
+    }
+    cout<<endl;
+    for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
+            board.column[i][j]=false;
+        }
+    }
+
+    for(int i=0;i<2;i++){
+        for(int j=0;j<2;j++){
+            for(int k=0;k<9;k++){
+                board.squares[i][j][k]=false;
+            }
+        }
     }
 
 }
